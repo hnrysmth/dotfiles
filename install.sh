17ea14c1ef63f9bc02c3964ddc6b96c16165123c
Henry Catalini Smith <hen@hey.com>
2020-09-21 22:12:15 +0200

    add install script

---
+++ install.sh
@@ -0,0 +1,21 @@
+#!/bin/zsh
+
+function dotfiles() {
+  git --git-dir=$HOME/.dotfiles/ --work-tree=$HOME $@
+}
+
+git clone --bare git@github.com:hencatsmith/dotfiles.git .dotfiles
+dotfiles config --local status.showUntrackedFiles no
+
+dotfiles sparse-checkout init
+dotfiles sparse-checkout set .gitconfig
+dotfiles sparse-checkout add .gitignore
+dotfiles sparse-checkout add .gitmodules
+dotfiles sparse-checkout add .vim
+dotfiles sparse-checkout add .vimrc
+dotfiles sparse-checkout add .zshrc
+dotfiles checkout
+
+dotfiles checkout trunk -- .vim
+dotfiles submodule update --init --recursive
+
